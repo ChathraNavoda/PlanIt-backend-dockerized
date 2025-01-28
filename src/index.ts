6ebaadbade41b12478ecd0d51cdb4bbef41121ac
ChathraNavoda <chathra96@gmail.com>
2025-01-28 17:01:03 +0530

    create new user

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use("/auth", authRouter);
 
 app.get("/", (req, res)=>{
-    res.send("Welcome to my app!. This is my 1st Docker project. I'm so excited! 😎❤️🍕😊🎈🎆🎇✨🎀");
+    res.send("Welcome to my app!. This is my 1st Docker project. I'm so excited! 😎❤️🍕😊🎈🎆🎇✨🎀. Hey!");
 });
 
 app.listen(8000, ()=> {
