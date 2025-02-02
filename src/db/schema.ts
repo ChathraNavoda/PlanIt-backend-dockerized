04a9ed8918f2f8f248e890f7dcccbc7ebdff41e8
ChathraNavoda <chathra96@gmail.com>
2025-02-02 17:20:45 +0530

    routes for create, get all, delete tasks

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,5 @@
 import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { title } from "process";
 
 export const users = pgTable("users", {
     id: uuid("id").primaryKey().defaultRandom(),
@@ -12,3 +13,17 @@ export const users = pgTable("users", {
 export type User = typeof users.$inferSelect;
 export type NewUser = typeof users.$inferInsert;
 
+
+export const tasks = pgTable("tasks", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    title: text("title").notNull(),
+    description: text("description").notNull(),
+    hexColor: text("hex_color").notNull(),
+    uid: uuid("uid").notNull().references(()=>users.id, {onDelete: "cascade"}),
+    dueAt: timestamp("due_at").$defaultFn(()=> new Date(Date.now() + 7*24*60*60*1000)),
+    createdAt: timestamp("created_at").defaultNow(),
+    updatedAt: timestamp("updatedAt").defaultNow()
+});
+
+export type Task = typeof tasks.$inferSelect;
+export type NewTask = typeof tasks.$inferInsert;
